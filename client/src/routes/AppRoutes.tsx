988ae0039a86cc9987be83c9c8e2201d7f995a2b
Arwa Mostafa <Arwa.Mostafa@tut.com>
2026-07-13 12:54:36 +0200

    feat: add main layout

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,13 +2,16 @@ import {Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
+import MainLayout from "../layouts/MainLayout";
 
 function AppRoutes() {
     return(
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Route>
         </Routes>
     )
 }
